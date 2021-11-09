fd027382b3d006c5ee2a26f7488a838bcfed4749
hemanth <hemanth9774@gmail.com>
2021-11-09 17:38:53 +0530

    my java assginment submission 1

---
+++ Capgemini/src/Generics/Question1.java
@@ -0,0 +1,12 @@
+package Generics;
+
+import java.util.HashSet;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+HashSet<employee> hs=new Hashset<employee>();
+hs.add()
+	}
+
+}
